bdbc82e4ec85bc0d30ca05ff2dc496182608d2a4
David André Soares Júlio <davidsjulio97@gmail.com>
2020-06-28 12:49:46 +0100

    - Criação da view do adapter para as consultas

--- app/src/main/java/com/davidjulio/consultamedica/adapter/AdapterAppoint.java
+++ app/src/main/java/com/davidjulio/consultamedica/adapter/AdapterAppoint.java
@@ -2,9 +2,13 @@ package com.davidjulio.consultamedica.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.davidjulio.consultamedica.R;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,7 +26,9 @@ public class AdapterAppoint extends RecyclerView.Adapter<AdapterAppoint.AppointV
     @NonNull
     @Override
     public AppointViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View listAppoint = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_appoint, parent, false);
+        return new AppointViewHolder( listAppoint );
     }
 
 
@@ -38,8 +44,15 @@ public class AdapterAppoint extends RecyclerView.Adapter<AdapterAppoint.AppointV
 
     public class AppointViewHolder extends RecyclerView.ViewHolder {
 
+        TextView  tvAppointDate, tvAppointHour, tvPatientID, tvPatientName, tvPatientResult;
         public AppointViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvAppointDate = itemView.findViewById(R.id.tvAppointDate);
+            tvAppointHour = itemView.findViewById(R.id.tvAppointHour);
+            tvPatientID = itemView.findViewById(R.id.tvPatientID);
+            tvPatientName = itemView.findViewById(R.id.tvPatientName);
+            tvPatientResult = itemView.findViewById(R.id.tvPatientResult);
         }
     }
 

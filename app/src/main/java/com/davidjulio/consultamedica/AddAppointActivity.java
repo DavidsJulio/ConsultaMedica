bb9d2ec19c954de62fa39d9e50acf89df80b4b53
David André Soares Júlio <davidsjulio97@gmail.com>
2020-06-28 11:59:22 +0100

    - activity_add_appoint.xml, código para a activity - Criação das strings correspondentes, incialização dos componentes na class

--- app/src/main/java/com/davidjulio/consultamedica/AddAppointActivity.java
+++ app/src/main/java/com/davidjulio/consultamedica/AddAppointActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class AddAppointActivity extends AppCompatActivity {
+
+    TextInputEditText tiDateAppoint, tiHourAppoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appoint);
+
+        tiDateAppoint = (TextInputEditText) findViewById(R.id.tiDateAppoint);
+        tiHourAppoint = (TextInputEditText) findViewById(R.id.tiHourAppoint);
     }
 }
